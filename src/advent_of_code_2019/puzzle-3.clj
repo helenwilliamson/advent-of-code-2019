14a1994de6d457d8355bfafc2d45decbc744d192
Helen Williamson <helenelizabethwilliamson@gmail.com>
2019-12-04 22:11:57 +0000

    puzzle-3.2

--- src/advent_of_code_2019/puzzle-3.clj
+++ src/advent_of_code_2019/puzzle-3.clj
@@ -13,16 +13,26 @@
     {:direction "D" :length 4}
     {:direction "L" :length 4}]])
 
+(def dummy2
+  ["R75,D30,R83,U83,L12,D49,R71,U7,L72"
+   "U62,R66,U55,R34,D71,R55,D58,R83"])
+
+(def dummy3
+  ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+   "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"])
+
 (defn read-input
-  []
-  (->> (slurp "resources/puzzle-3")
-       (str/split-lines)
-       (map
-        (fn [line]
-          (->> (str/split line #",")
-               (map #(hash-map
-                      :direction (subs %1 0 1)
-                      :length (Integer/parseInt (subs %1 1)))))))))
+  ([] (-> (slurp "resources/puzzle-3")
+          str/split-lines
+          read-input))
+  ([in]
+   (->> in
+        (map
+         (fn [line]
+           (->> (str/split line #",")
+                (map #(hash-map
+                       :direction (subs %1 0 1)
+                       :length (Integer/parseInt (subs %1 1))))))))))
 
 (defn up
   [length x y]
@@ -47,7 +57,6 @@
 (defn draw
   [grid point {:keys [direction length]}]
   (let [{:keys [x y]} point]
-    ;(println length x y)
     (case direction
       "U" (up length x y)
       "D" (down length x y)
@@ -57,12 +66,12 @@
 (defn lay-wire-pieces
   [input]
   (loop [wire-pieces input
-         grid #{}
+         grid []
          point {:x 0 :y 0}]
     (let [[wire-piece & remaining-wire-pieces] wire-pieces
           next-line (draw grid point wire-piece)
           next-point (first next-line)
-          next-grid (st/union grid (into #{} next-line))]
+          next-grid (concat grid (reverse next-line))]
       ;(println wire-piece)
       ;(println next-line)
       ;(println next-grid)
@@ -78,8 +87,19 @@
   [input]
   (let [red-points (lay-wire-pieces (first input))
         blue-points (lay-wire-pieces (second input))
-        crossing-points (disj (st/intersection red-points blue-points) {:x 0 :y 0})]
+        crossing-points (disj (st/intersection (into #{} red-points) (into #{} blue-points)) {:x 0 :y 0})]
     (->> crossing-points
          (map #(+ (Math/abs (:x %1)) (Math/abs (:y %1))))
          sort
          first)))
+
+(defn closed-crossing-by-steps
+  [input]
+  (let [red-points (lay-wire-pieces (first input))
+        blue-points (lay-wire-pieces (second input))
+        crossing-points (disj (st/intersection (into #{} red-points) (into #{} blue-points)) {:x 0 :y 0})]
+    (->> crossing-points
+         (map #(+ 2 (.indexOf red-points %1) (.indexOf blue-points %1)))
+         sort
+         ;first
+         )))
